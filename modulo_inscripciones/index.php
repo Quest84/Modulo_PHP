8728dabd7ec2388cd7b21bdf9b725e9d4d270306
Quest84 <QuestionSleep123@gmail.com>
2021-01-06 13:08:16 -0600

    Primer Commit CSS

--- modulo_inscripciones/index.php
+++ modulo_inscripciones/index.php
@@ -1,45 +1,92 @@
 <!DOCTYPE html>
 <html>
+<style>
+L{
+	font-size:18px;
+}
+
+M{
+	font-size:26px;
+}
+body{
+	background-image: url("tesjo.png");
+	background-repeat: no-repeat;
+  	background-attachment: fixed;
+  	background-position: center;
+
+}
+a:link {
+  color: grey;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:visited {
+  color: white;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:hover {
+  color: red;
+  background-color: transparent;
+  text-decoration: underline;
+}
+a:active {
+  color: yellow;
+  background-color: transparent;
+  text-decoration: underline;
+}
+</style>
 <head>
 		<title>Menú del Modulo</title>
+		<link rel="stylesheet" type="text/css" href="estilos.css">
+		<meta charset="utf-8">
+		<style type="text/css">
+			table, th, td {
+				border: 1px solid black;
+				border-collapse: collapse;
+			}
+			th, td{
+				padding: 10px;
+			}
+		</style>
 </head>
 <body>
 	<center>
-		<form method = "LINK" action = "https://miapp5245588.000webhostapp.com/home.html">
+	<font size="14" face="roboto" color=white>
+	<table style="width: 50%" border = "0" bgcolor="BLACK">
+		<thead>
+			<tr>
+				<th colspan = "10"><M>MÓDULO INSCRIPCIONES<M></th>
+			</tr>
+		</thead>
+	</table>
+	</center>
+</font>
+		<form align="center" method = "LINK" action = "https://miapp5245588.000webhostapp.com/home.html">
 				<input type = "submit" value = "Regresar">
-			</form>		
-<body>
+			</form>	
+	<center>
+	
+<body>	
+	<font size="14" face="roboto" color=white>
 		<center>
-			<table border = "3">
-				<thead>
-					<tr>
-						<th colspan = "10">Módulo de Inscripciones</th>
-					</tr>
-				</thead>
-			</tbody>
-					<td><center><a href = "tabla_Pagos.php">Pagos</center></td>
+			<table style="width: 20%" border = "0" bgcolor="black">
 
+				<th><center><a href = "tabla_Pagos.php"><L>Pagos</L></center></th>
 				</tr>
-				
 				<tr>
-					<td><center><a href = "tabla_cargaAcad.php">Carga Academica</center></td>
-
+				<th><center><a href = "tabla_cargaAcad.php"><L>Carga Academica</L></center></th>
 				</tr>
-				
 				<tr>
-					<td><center><a href = "tabla_Inscripciones.php">Inscripciones</center></td>
-
+				<th><center><a href = "tabla_Inscripciones.php"><L>Inscripciones</L></center></th>
 				</tr>
-				
 				<tr>
-					<td><center><a href = "tabla_InscPagos.php">Inscripciones-Pagos</center></td>
-
+				<th><center><a href = "tabla_InscPagos.php"><L>Inscripciones-Pagos</L></center></th>
 				</tr>
-				
 				<tr>
-					<td><center><a href = "tabla_MatCarga.php">Materias-Carga</center></td>
-				
+				<th><center><a href = "tabla_MatCarga.php"><L>Materias-Carga</L></center></th>
 			</a>
 		</center>
 	</td>
+</body>
 </tr>
