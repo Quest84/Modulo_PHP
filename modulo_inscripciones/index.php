b72650526f12642ebb27b2aa2d848e2d9c345245
Quest84 <QuestionSleep123@gmail.com>
2021-01-06 00:26:11 -0600

    index añadido

---
+++ modulo_inscripciones/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title>Menú del Modulo</title>
+</head>
+<body>
+	<center>
+		<form method = "LINK" action = "https://miapp5245588.000webhostapp.com/home.html">
+				<input type = "submit" value = "Regresar">
+			</form>		
+<body>
+		<center>
+			<table border = "3">
+				<thead>
+					<tr>
+						<th colspan = "10">Módulo de Inscripciones</th>
+					</tr>
+				</thead>
+			</tbody>
+					<td><center><a href = "tabla_Pagos.php">Pagos</center></td>
+
+				</tr>
+				
+				<tr>
+					<td><center><a href = "tabla_cargaAcad.php">Carga Academica</center></td>
+
+				</tr>
+				
+				<tr>
+					<td><center><a href = "tabla_Inscripciones.php">Inscripciones</center></td>
+
+				</tr>
+				
+				<tr>
+					<td><center><a href = "tabla_InscPagos.php">Inscripciones-Pagos</center></td>
+
+				</tr>
+				
+				<tr>
+					<td><center><a href = "tabla_MatCarga.php">Materias-Carga</center></td>
+				
+			</a>
+		</center>
+	</td>
+</tr>
