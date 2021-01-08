94b4efc3a2a0befb61ff10e8c48e5f1a62173ff8
Quest84 <QuestionSleep123@gmail.com>
2021-01-08 05:33:37 -0600

    Cambios menores

--- modulo_inscripciones/OP_Generar_CargaAcad.php
+++ modulo_inscripciones/OP_Generar_CargaAcad.php
@@ -4,13 +4,17 @@
 	$id = $_REQUEST['id'];
 	$id2 = $_REQUEST['id2'];
 
-	$query = "SELECT m.nombre, m.clave_materia, m.creditos FROM materias AS m INNER JOIN materias_carga AS c ON c.clave_materia = m.clave_materia and c.id_carga_acad = '$id';";
+	$queryMaterias = "SELECT m.nombre, m.clave_materia, m.creditos FROM materias AS m INNER JOIN materias_carga AS c ON c.clave_materia = m.clave_materia and c.id_carga_acad = '$id';";
 
-	$query2 = "SELECT num_control FROM carga_academica WHERE num_control = '$id2'";
+	$queryControl = "SELECT num_control FROM carga_academica WHERE num_control = '$id2'";
 
-	$resultado = $conexion->query($query) or die($conexion->error);
+	$queryNombre = "SELECT p.nombre, p.apellido_paterno, p.apellido_materno FROM persona AS p INNER JOIN alumnos AS a ON p.id_persona = a.id_persona and a.num_control = '$id2'";
+
+	$resultadoMaterias = $conexion->query($queryMaterias) or die($conexion->error);
 	
-	$resultado2 = $conexion->query($query2) or die($conexion->error);
+	$resultadoControl = $conexion->query($queryControl) or die($conexion->error);
+
+	$resultadoNombre = $conexion->query($queryNombre) or die($conexion->error);
 
 	require 'fpdf/fpdf.php';
 	
@@ -34,10 +38,10 @@
 		   	$this->Cell(0,0,utf8_decode('Tecnológico de Estudios Superiores de Jocotitlán'),0,1,'C');
 		   	$this->Ln(8);
 		   	$this->SetFont('Arial','B',14);
-		   	$this->Cell(0,0,utf8_decode('Carga Académica del alumno: '),0,1,'C');
-		   	
+		   	$this->Cell(0,0,utf8_decode('CARGA ACADÉMICA'),0,1,'C');
+		   	$this->Ln(8);
 		    // Salto de línea
-		    $this->Ln(4);
+		    // $this->Ln(4);
 		}
 	
 		// Pie de página
@@ -56,25 +60,41 @@
 	$pdf = new PDF();
 	$pdf->AliasNbPages();
 	$pdf->AddPage();
-	$pdf->Cell(70);
-	while($row = $resultado2->fetch_assoc())
+
+	$pdf->Ln(4);
+	$pdf->Cell(-10);
+	$pdf->Cell(50,6,'Alumno: ',0,0,'C');
+	while($row = $resultadoNombre->fetch_assoc())
 	{
+		$pdf->Cell(-30);
+		$pdf->Cell(50,6,utf8_decode($row['nombre']),0,0,'C');
+		$pdf->Cell(-30);
+		$pdf->Cell(50,6,utf8_decode($row['apellido_paterno']),0,0,'C');
+		$pdf->Cell(-30);
+		$pdf->Cell(50,6,utf8_decode($row['apellido_materno']),0,0,'C');
+	}
+	$pdf->Ln(8);
+	$pdf->Cell(3);
+	$pdf->Cell(50,6,'Numero de Control: ',0,0,'C');
+	while($row = $resultadoControl->fetch_assoc())
+	{
+		$pdf->Cell(-8);
 		$pdf->Cell(50,6,utf8_decode($row['num_control']),0,0,'C');
 	}
 	$pdf->Ln(8);
 	$pdf->SetFillColor(232,232,232);
 	$pdf->SetFont('Arial','B',12);
-	$pdf->Cell(80,6,'Nombre',1,0,'C',1);
 	$pdf->Cell(60,6,'Clave de la Materia',1,0,'C',1);
-	$pdf->Cell(40,6,'Creditos',1,1,'C',1);
+	$pdf->Cell(80,6,'Nombre',1,0,'C',1);
+	$pdf->Cell(50,6,'Creditos',1,1,'C',1);
 	
 	$pdf->SetFont('Arial','',10);
 	
-	while($row = $resultado->fetch_assoc())
+	while($row = $resultadoMaterias->fetch_assoc())
 	{
-		$pdf->Cell(80,6,utf8_decode($row['nombre']),1,0,'C');
 		$pdf->Cell(60,6,$row['clave_materia'],1,0,'C');
-		$pdf->Cell(40,6,utf8_decode($row['creditos']),1,1,'C');
+		$pdf->Cell(80,6,utf8_decode($row['nombre']),1,0,'C');
+		$pdf->Cell(50,6,utf8_decode($row['creditos']),1,1,'C');
 	}
 	$pdf->Output();
 ?>
